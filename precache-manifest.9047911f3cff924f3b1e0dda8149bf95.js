bf4444a2d3ad091f151ce48f785174337f47dc81
qqppgwa <qqppgwa@gmail.com>
2019-11-02 16:56:13 +0800

    Updates

---
+++ precache-manifest.9047911f3cff924f3b1e0dda8149bf95.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f7818db38f1b38797de92fcbb2c19d",
+    "url": "/react-freecell/index.html"
+  },
+  {
+    "revision": "57b8d9fd76863ea2c223",
+    "url": "/react-freecell/static/css/main.7984bdef.chunk.css"
+  },
+  {
+    "revision": "7e956129ee7be4517098",
+    "url": "/react-freecell/static/js/2.aace2cc9.chunk.js"
+  },
+  {
+    "revision": "57b8d9fd76863ea2c223",
+    "url": "/react-freecell/static/js/main.3a3ba1f6.chunk.js"
+  },
+  {
+    "revision": "56e552aefefc2f80a269",
+    "url": "/react-freecell/static/js/runtime-main.113b4abb.js"
+  },
+  {
+    "revision": "903e94a633280582422c0176983f8620",
+    "url": "/react-freecell/static/media/bg-all.903e94a6.png"
+  },
+  {
+    "revision": "99039dbe6ff5eb9620f28253cd882e22",
+    "url": "/react-freecell/static/media/club_11.99039dbe.png"
+  },
+  {
+    "revision": "35cea76e179da611f142b12672aad7b1",
+    "url": "/react-freecell/static/media/club_12.35cea76e.png"
+  },
+  {
+    "revision": "62878c8ba300cf63bcc5572b09317e21",
+    "url": "/react-freecell/static/media/club_13.62878c8b.png"
+  },
+  {
+    "revision": "e713937b518d957cb748992baf0f8780",
+    "url": "/react-freecell/static/media/diamond_11.e713937b.png"
+  },
+  {
+    "revision": "b0a8a1a376b09eecc60c42e86b0022f4",
+    "url": "/react-freecell/static/media/diamond_12.b0a8a1a3.png"
+  },
+  {
+    "revision": "5f124f05af635261395c62cdf87a9d9f",
+    "url": "/react-freecell/static/media/diamond_13.5f124f05.png"
+  },
+  {
+    "revision": "c9c8ef0ec9d09902af4a6d43d961c2e8",
+    "url": "/react-freecell/static/media/heart_11.c9c8ef0e.png"
+  },
+  {
+    "revision": "be3296c4855f934946d078478db31649",
+    "url": "/react-freecell/static/media/heart_12.be3296c4.png"
+  },
+  {
+    "revision": "7ea88201883560e13f5bcc44d26f06d2",
+    "url": "/react-freecell/static/media/heart_13.7ea88201.png"
+  },
+  {
+    "revision": "c1e2e178d04cdebf6ea3281e5c4e0919",
+    "url": "/react-freecell/static/media/spade_11.c1e2e178.png"
+  },
+  {
+    "revision": "9c935ab9fdad8e858ddde49f392945c7",
+    "url": "/react-freecell/static/media/spade_12.9c935ab9.png"
+  },
+  {
+    "revision": "3e016a3a1a284c5eaca9e46378f312f8",
+    "url": "/react-freecell/static/media/spade_13.3e016a3a.png"
+  }
+]);
